6fff24585133f55ffd8671aa94dfa1ef876fefb9
lidijaf <lidija.fodor@dmi.uns.ac.rs>
2016-03-08 11:21:06 +0100

    This is my first YODIZ integrated commit @T132

--- src/test/TestClass.java
+++ src/test/TestClass.java
@@ -2,7 +2,7 @@ package test;
 
 public class TestClass{
 	public static void main(String[] args){
-		System.out.println("Hello PRIS and YODIZ world!");
+		System.out.println("Hello PRIS and YODIZ world xxxxx!");
 	}
 
 }
